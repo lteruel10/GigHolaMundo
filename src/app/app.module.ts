cf59dcdbc412b9b5da724450c7603cbe986a29bf
lteruel10 <lteruel10@gmail.com>
2021-07-22 17:44:20 -0600

    43. Creando el footer.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './components/header/header.components'
-import {BodyComponent} from './components/body/body.component'
+import {BodyComponent} from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent, BodyComponent ],
+    HeaderComponent, BodyComponent, FooterComponent ],
   imports: [
     BrowserModule
   ],
